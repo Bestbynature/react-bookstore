874100a1d11472ec718c31f282e708db7df031cb
Dammy <bestbynature2010@gmail.com>
2023-05-08 09:02:23 +0100

    :zap: Add useSelector hook to access the state

--- src/components/bookstore.js
+++ src/components/bookstore.js
@@ -1,35 +1,10 @@
-import { useState } from 'react';
 import Booklist from './booklist';
 import Form from './form';
 
-const Bookstore = () => {
-  const [books, setBooks] = useState([
-    {
-      id: 1, title: 'The Hunger Games', author: 'Suzanne Collins', genre: 'Action', percent: '64', chapter: 'Chapter 17',
-    },
-    {
-      id: 2, title: 'Harry Potter and the Order of the Phoenix', author: 'J.K. Rowling', genre: 'Fantasy', percent: '8', chapter: 'Chapter 3: A lesson learned',
-    },
-    {
-      id: 3, title: 'To Kill a Mockingbird', author: 'Harper Lee', genre: 'scifi', percent: '0', chapter: 'Introduction',
-    },
-  ]);
-
-  const handleForm = (e) => {
-    e.preventDefault();
-    const title = e.target[0].value;
-    const author = e.target[1].value;
-    const book = { title, author };
-    setBooks([...books, book]);
-    e.target.reset();
-  };
-
-  return (
-    <div className="bookstore">
-      <Booklist books={books} />
-      <Form handleForm={handleForm} />
-    </div>
-  );
-};
-
+const Bookstore = () => (
+  <div className="bookstore">
+    <Booklist />
+    <Form />
+  </div>
+);
 export default Bookstore;
