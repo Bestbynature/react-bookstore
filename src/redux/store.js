60226f0620bb4c468a54c90d28a86435f1ac14cc
Dammy <bestbynature2010@gmail.com>
2023-05-08 16:08:57 +0100

    :shipit: Import the reducer for the categories page

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
+    categories: categoriesReducer,
   },
 });
 
