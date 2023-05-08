93440347efeaeecee93df952ad93a9f034122a8c
Dammy <bestbynature2010@gmail.com>
2023-05-08 09:51:35 +0100

    :rocket: Implement useEffect hook to re-render upon data retrieval from the API

--- src/components/booklist.js
+++ src/components/booklist.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Bookitem from './Bookitem';
+import { getBooks } from '../redux/books/booksSlice';
 
 const Booklist = () => {
-  const { books } = useSelector((store) => store.books);
+  const dispatch = useDispatch();
+
+  const { books, isLoading, count } = useSelector((store) => store.books);
+
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch, count]);
 
   return (
     <div className="booklist">
+      <h2>{isLoading && <p>Loading...</p>}</h2>
       <ul>
+        {Object.keys(books).length === 0 && !isLoading && <p>No books available</p>}
 
-        {books.map((book) => <Bookitem key={book.item_id} book={book} />)}
+        {Object.keys(books).map((key) => {
+          const book = books[key][0];
+          return <Bookitem key={key} book={book} id={key} />;
+        })}
 
       </ul>
     </div>
